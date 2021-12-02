12bb9263c02ec010836a221105b0f410a4014f50
Anthony Chavez <anthony@anthonychavez.xyz>
2021-12-02 12:11:12 -0600

    Updated Emacs config and .gitconfig
    Removed doom modeline and org-beautify-theme for now

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -133,13 +133,13 @@
 
   ;; Enable flashing mode-line on errors
   ;; (doom-themes-visual-bell-config)
-  
+
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   ;; (doom-themes-neotree-config)
   ;; or for treemacs users
   (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
   (doom-themes-treemacs-config)
-  
+
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
@@ -180,3 +180,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'upcase-region 'disabled nil)

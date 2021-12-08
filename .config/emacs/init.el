e19092cfae06257a2990c5e648fc3817eb4cbd96
Anthony Chavez <anthony@anthonychavez.xyz>
2021-12-07 19:21:48 -0600

    Emacs config updates

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -38,86 +38,87 @@
 ;;   :config
 ;;   (load-theme 'alect-black t))
 
-;; (use-package modus-themes
-;;   :straight t
-;;   :init
-;;   ;; Add all your customizations prior to loading the themes
-;; (setq modus-themes-italic-constructs t
-;;       modus-themes-bold-constructs nil
-;;       modus-themes-mixed-fonts t
-;;       modus-themes-subtle-line-numbers t
-;;       modus-themes-intense-markup t
-;;       modus-themes-success-deuteranopia t
-;;       modus-themes-tabs-accented t
-;;       modus-themes-inhibit-reload t ; only applies to `customize-set-variable' and related
+(use-package modus-themes
+  :straight t
+  :init
+  ;; Add all your customizations prior to loading the themes
+(setq modus-themes-italic-constructs t
+      modus-themes-bold-constructs nil
+      modus-themes-mixed-fonts t
+      modus-themes-subtle-line-numbers t
+      modus-themes-intense-markup t
+      modus-themes-success-deuteranopia t
+      modus-themes-tabs-accented t
+      modus-themes-inhibit-reload t ; only applies to `customize-set-variable' and related
 
-;;       modus-themes-fringes nil ; {nil,'subtle,'intense}
+      modus-themes-fringes nil ; {nil,'subtle,'intense}
 
-;;       ;; Options for `modus-themes-lang-checkers' are either nil (the
-;;       ;; default), or a list of properties that may include any of those
-;;       ;; symbols: `straight-underline', `text-also', `background',
-;;       ;; `intense' OR `faint'.
-;;       modus-themes-lang-checkers nil
+      ;; Options for `modus-themes-lang-checkers' are either nil (the
+      ;; default), or a list of properties that may include any of those
+      ;; symbols: `straight-underline', `text-also', `background',
+      ;; `intense' OR `faint'.
+      modus-themes-lang-checkers nil
 
-;;       ;; Options for `modus-themes-mode-line' are either nil, or a list
-;;       ;; that can combine any of `3d' OR `moody', `borderless',
-;;       ;; `accented', `padded'.
-;;       modus-themes-mode-line '(accented borderless)
+      ;; Options for `modus-themes-mode-line' are either nil, or a list
+      ;; that can combine any of `3d' OR `moody', `borderless',
+      ;; `accented', `padded'.
+      modus-themes-mode-line '(accented borderless)
 
-;;       ;; Options for `modus-themes-syntax' are either nil (the default),
-;;       ;; or a list of properties that may include any of those symbols:
-;;       ;; `faint', `yellow-comments', `green-strings', `alt-syntax'
-;;       modus-themes-syntax nil
+      ;; Options for `modus-themes-syntax' are either nil (the default),
+      ;; or a list of properties that may include any of those symbols:
+      ;; `faint', `yellow-comments', `green-strings', `alt-syntax'
+      modus-themes-syntax nil
 
-;;       ;; Options for `modus-themes-hl-line' are either nil (the default),
-;;       ;; or a list of properties that may include any of those symbols:
-;;       ;; `accented', `underline', `intense'
-;;       modus-themes-hl-line '(accented)
+      ;; Options for `modus-themes-hl-line' are either nil (the default),
+      ;; or a list of properties that may include any of those symbols:
+      ;; `accented', `underline', `intense'
+      modus-themes-hl-line nil
 
-;;       ;; Options for `modus-themes-paren-match' are either nil (the
-;;       ;; default), or a list of properties that may include any of those
-;;       ;; symbols: `bold', `intense', `underline'
-;;       modus-themes-paren-match '(bold intense)
+      ;; Options for `modus-themes-paren-match' are either nil (the
+      ;; default), or a list of properties that may include any of those
+      ;; symbols: `bold', `intense', `underline'
+      modus-themes-paren-match '(bold intense)
 
-;;       ;; Options for `modus-themes-links' are either nil (the default),
-;;       ;; or a list of properties that may include any of those symbols:
-;;       ;; `neutral-underline' OR `no-underline', `faint' OR `no-color',
-;;       ;; `bold', `italic', `background'
-;;       modus-themes-links '(background)
+      ;; Options for `modus-themes-links' are either nil (the default),
+      ;; or a list of properties that may include any of those symbols:
+      ;; `neutral-underline' OR `no-underline', `faint' OR `no-color',
+      ;; `bold', `italic', `background'
+      modus-themes-links '(background)
 
-;;       ;; Options for `modus-themes-prompts' are either nil (the
-;;       ;; default), or a list of properties that may include any of those
-;;       ;; symbols: `background', `bold', `gray', `intense', `italic'
-;;       modus-themes-prompts '(intense bold)
+      ;; Options for `modus-themes-prompts' are either nil (the
+      ;; default), or a list of properties that may include any of those
+      ;; symbols: `background', `bold', `gray', `intense', `italic'
+      modus-themes-prompts '(intense bold)
 
-;;       modus-themes-completions 'moderate ; {nil,'moderate,'opinionated}
+      modus-themes-completions 'moderate ; {nil,'moderate,'opinionated}
 
-;;       modus-themes-mail-citations nil ; {nil,'faint,'monochrome}
+      modus-themes-mail-citations nil ; {nil,'faint,'monochrome}
 
-;;       ;; Options for `modus-themes-region' are either nil (the default),
-;;       ;; or a list of properties that may include any of those symbols:
-;;       ;; `no-extend', `bg-only', `accented'
-;;       modus-themes-region '(bg-only no-extend)
+      ;; Options for `modus-themes-region' are either nil (the default),
+      ;; or a list of properties that may include any of those symbols:
+      ;; `no-extend', `bg-only', `accented'
+      modus-themes-region '(bg-only no-extend)
 
-;;       ;; Options for `modus-themes-diffs': nil, 'desaturated,
-;;       ;; 'bg-only, 'deuteranopia, 'fg-only-deuteranopia
-;;       modus-themes-diffs 'fg-only-deuteranopia
+      ;; Options for `modus-themes-diffs': nil, 'desaturated,
+      ;; 'bg-only, 'deuteranopia, 'fg-only-deuteranopia
+      modus-themes-diffs 'fg-only-deuteranopia
 
-;;       modus-themes-org-blocks 'gray-background ; {nil,'gray-background,'tinted-background}
+      modus-themes-org-blocks 'gray-background ; {nil,'gray-background,'tinted-background}
 
-;;       modus-themes-org-agenda ; this is an alist: read the manual or its doc string
-;;       '((header-block . (variable-pitch scale-title))
-;;         (header-date . (grayscale workaholic bold-today))
-;;         (event . (accented scale-small))
-;;         (scheduled . uniform)
-;;         (habit . traffic-light-deuteranopia)))
+      modus-themes-org-agenda ; this is an alist: read the manual or its doc string
+      '((header-block . (variable-pitch scale-title))
+        (header-date . (grayscale workaholic bold-today))
+        (event . (accented scale-small))
+        (scheduled . uniform)
+        (habit . traffic-light-deuteranopia)))
 
-;;   ;; Load the theme files before enabling a theme (else you get an error).
-;;   (modus-themes-load-themes)
-;;   :config
-;;   ;; Load the theme of your choice:
-;;   (modus-themes-load-vivendi) ;; OR (modus-themes-load-vivendi)
-;;   :bind ("<f5>" . modus-themes-toggle))
+  ;; Load the theme files before enabling a theme (else you get an error).
+;;  (modus-themes-load-themes)
+  :config
+  ;; Load the theme of your choice:
+;;  (modus-themes-load-vivendi) ;; OR (modus-themes-load-vivendi)
+;;  :bind ("<f5>" . modus-themes-toggle)
+  )
 
 (use-package doom-themes
    :straight t
@@ -125,8 +126,8 @@
 ;;   ;; Global settings (defaults)
    (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
          doom-themes-enable-italic t) ; if nil, italics is universally disabled
-   (load-theme 'doom-acario-dark t)
-   ;; (load-theme 'doom-tomorrow-night t)
+   ;; (load-theme 'doom-acario-dark t)
+   (load-theme 'doom-tomorrow-night t)
    ;; (load-theme 'doom-molokai t)
    ;; (load-theme 'doom-snazzy t)
    ;; (load-theme 'doom-horizon t)
@@ -156,13 +157,22 @@
   ;; Default directory for org files (not all are stored here).
   (setq org-directory "~/Nextcloud/Documents/Notes/Org")
 
-  (setq org-log-done t)
-  (setq org-return-follows-link t)
+  (setq org-log-done t
+        org-return-follows-link t
+        org-src-fontify-natively t   ;; Pretty code blocks
+        org-src-tab-acts-natively t)
   (add-hook 'org-mode-hook 'org-indent-mode)
   (add-hook 'org-mode-hook
             '(lambda ()
               (visual-line-mode 1)))
-  (add-hook 'org-mode-hook 'org-bullets-mode)))
+  (add-hook 'org-mode-hook 'org-bullets-mode))
+(org-babel-do-load-languages 'org-babel-load-languages
+    '((shell . t)
+      (python . t)
+      (C . t)
+      (makefile . t)
+      (sql . t)
+      (sqlite . t))))
 
 ;; Load config.org for init.el configuration
 (org-babel-load-file (expand-file-name "~/.config/emacs/config.org"))
@@ -172,8 +182,44 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#171717" "#aa4450" "#87875f" "#cc8800" "#87AFD7" "#8787AF" "#87ceeb" "#c2c2b0"])
+ '(custom-safe-themes
+   '("cbdf8c2e1b2b5c15b34ddb5063f1b21514c7169ff20e081d39cf57ffee89bc1e" "9b54ba84f245a59af31f90bc78ed1240fca2f5a93f667ed54bbf6c6d71f664ac" "4b0e826f58b39e2ce2829fab8ca999bcdc076dec35187bf4e9a4b938cb5771dc" "31deed4ac5d0b65dc051a1da3611ef52411490b2b6e7c2058c13c7190f7e199b" default))
  '(elfeed-feeds
-   '("https://protesilaos.com/advice.xml" "https://protesilaos.com/feeds/")))
+   '("https://protesilaos.com/advice.xml" "https://protesilaos.com/feeds/"))
+ '(exwm-floating-border-color "#1d2127")
+ '(fci-rule-color "#62686E")
+ '(highlight-tail-colors ((("#22221e") . 0) (("#22292c") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#1d2127" "#87ceeb"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1d2127" "#87875f"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1d2127" "#686858"))
+ '(objed-cursor-color "#aa4450")
+ '(pdf-view-midnight-colors (cons "#c2c2b0" "#171717"))
+ '(rustic-ansi-faces
+   ["#171717" "#aa4450" "#87875f" "#cc8800" "#87AFD7" "#8787AF" "#87ceeb" "#c2c2b0"])
+ '(vc-annotate-background "#171717")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#87875f")
+    (cons 40 "#9e873f")
+    (cons 60 "#b5871f")
+    (cons 80 "#cc8800")
+    (cons 100 "#dd8d00")
+    (cons 120 "#ee9200")
+    (cons 140 "#ff9800")
+    (cons 160 "#d7923a")
+    (cons 180 "#af8c74")
+    (cons 200 "#8787AF")
+    (cons 220 "#92708f")
+    (cons 240 "#9e5a6f")
+    (cons 260 "#aa4450")
+    (cons 280 "#994d51")
+    (cons 300 "#895654")
+    (cons 320 "#785f55")
+    (cons 340 "#62686E")
+    (cons 360 "#62686E")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -181,3 +227,4 @@
  ;; If there is more than one, they won't work right.
  )
 (put 'upcase-region 'disabled nil)
+(put 'scroll-left 'disabled nil)

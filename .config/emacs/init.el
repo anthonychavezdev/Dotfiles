743b668f65490f40ac5d4c58e3a35a20b5a6cca6
Anthony Chavez <anthony@anthonychavez.xyz>
2024-03-24 14:42:54 -0500

    Enable a few disabled commands

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -208,3 +208,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(put 'downcase-region 'disabled nil)
+(put 'narrow-to-region 'disabled nil)

00e0754270820c5c8d56b47f0747129fff97005f
Anthony Chavez <anthony@anthonychavez.xyz>
2024-09-14 20:16:55 -0500

    Add unzip package

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -156,6 +156,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+     unzip
      git
      nextcloud-client
      valent

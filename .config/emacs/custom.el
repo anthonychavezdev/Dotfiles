036e5c7796de55271570d971f2662d054e3fbd02
Anthony Chavez <anthony@anthonychavez.xyz>
2025-03-03 20:26:56 -0600

    Add updated custom.el file

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -4,7 +4,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ignored-local-variable-values '((vc-prepare-patches-separately)))
  '(safe-local-variable-values
    '((vc-default-patch-addressee . "bug-gnu-emacs@gnu.org")
      (etags-regen-ignores "test/manual/etags/")

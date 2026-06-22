4d909e7350d51490f04f44d856155c45f94c63c3
Anthony Chavez <anthony@anthonychavez.xyz>
2026-06-22 13:27:11 -0500

    Reformat the firewall section

--- nixos/hosts/jupiter/default.nix
+++ nixos/hosts/jupiter/default.nix
@@ -250,23 +250,31 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
   # networking.firewall.allowedUDPPorts = [ 8081 ];
-  networking.firewall.allowedTCPPorts = [
-  # Spotify
-  # sync local tracks from your filesystem with mobile devices in the same network
-    57621
-    # Expo
-    8081 ];
-  networking.firewall.allowedUDPPorts = [
-    # In order to enable discovery of Google Cast devices (and possibly other Spotify Connect devices) in the same network
-    5353 ];
+  networking.firewall = {
+    # Allow LAN connections
+    # extraCommands = ''
+    #   iptables -A nixos-fw -p tcp -s 192.168.1.0/24 -j nixos-jw-accept
+    #   iptables -A nixos-fw -p udp -s 192.168.1.0/24 -j nixos-jw-accept
+    #   '';
 
-  # KDE/GSconnect
-  networking.firewall.allowedTCPPortRanges = [
-    { from = 1714; to = 1764; }
-  ];
-  networking.firewall.allowedUDPPortRanges = [
-    { from = 1714; to = 1764; }
-  ];
+    allowedTCPPorts = [
+      # Spotify
+      # sync local tracks from your filesystem with mobile devices in the same network
+      57621
+      # Expo
+      8081 ];
+    allowedUDPPorts = [
+      # In order to enable discovery of Google Cast devices (and possibly other Spotify Connect devices) in the same network
+      5353 ];
+
+    # KDE/GSconnect
+    allowedTCPPortRanges = [
+      { from = 1714; to = 1764; }
+    ];
+    allowedUDPPortRanges = [
+      { from = 1714; to = 1764; }
+    ];
+  };
 
   # Fonts
   fonts.packages = with pkgs; [
